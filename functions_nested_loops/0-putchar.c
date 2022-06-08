982f0067d1c7e435791de2804489730ca8f0df23
Hardbass1991 <midailsorridente@gmail.com>
2022-06-07 18:30:56 -0700

    added .c script that prints characters using a non-predefined function

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Prints "_putchar"
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char text[] = "_putchar";
+	int n, i;
+
+	n = sizeof(text) / sizeof(text[0]);
+	for (i = 0; i < n; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+	return (0);
+}

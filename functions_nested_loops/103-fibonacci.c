fb2bfaa6eba46dfb9b474fd981b4f439a8414d90
Hardbass1991 <midailsorridente@gmail.com>
2022-06-09 10:59:22 -0700

    added .c script that prints out the sum of even Fibonacci numbers lower than 4000000

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - prints out the sum of even Fibonacci numbers lower than 4000000
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int i, a, b, sum;
+
+	a = 1, b = 2, i = 0, sum = 0;
+	while (a <= 4000000 && b <= 4000000)
+	{
+		if (i % 2 == 0)
+		{
+			a += b;
+			if (a % 2 == 0)
+				sum += a;
+		}
+		else
+		{
+			b += a;
+			if (b % 2 == 0)
+				sum += b;
+		}
+
+		i++;
+	}
+	printf("%ld", sum);
+	putchar('\n');
+	return (0);
+}

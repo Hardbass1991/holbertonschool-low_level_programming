d2fb5db5f85bc50a97a7d874f9c023a3a1f3515c
Hardbass1991 <midailsorridente@gmail.com>
2022-06-13 10:09:58 -0700

    added .c script that prints '\' and input n amount of times, in different lines

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,7 @@
 void print_diagonal(int n)
 {
 	int i, j;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
@@ -17,5 +17,6 @@ void print_diagonal(int n)
 		_putchar(92);
 		_putchar('\n');
 	}
-	_putchar('\n');
+	if (n <= 0)
+		_putchar('\n');
 }

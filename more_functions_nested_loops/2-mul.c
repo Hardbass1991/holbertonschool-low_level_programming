35d8bc96ded3c756a0ca639e4a07530cfc1191c8
Hardbass1991 <midailsorridente@gmail.com>
2022-06-13 08:30:40 -0700

    added .c script that returns the product of two input ints

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Checks whether input int is a digit 0-9
+ * @a: first input int
+ * @b: second input int
+ *
+ * Return: product of both input ints
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

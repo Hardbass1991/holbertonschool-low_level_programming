0d8b080cd324efb0abbdda0b6efd4e42b747df68
Hardbass1991 <midailsorridente@gmail.com>
2022-06-14 13:42:58 -0700

    added .c script that converts some characters of input string to 'leet'

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+/**
+ * *leet - switches characters in input string
+ * @a: input string
+ *
+ * Return: converted string
+ */
+char *leet(char *a)
+{
+	int i = 0, j;
+	char lo[] = {'a', 'e', 'o', 't', 'l'};
+	char up[] = {'A', 'E', 'O', 'T', 'L'};
+	char le[] = {'4', '3', '0', '7', '1'};
+
+	while (a[i])
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (a[i] == lo[j] || a[i] == up[j])
+				a[i] = le[j];
+		}
+		i++;
+	}
+	return (a);
+}

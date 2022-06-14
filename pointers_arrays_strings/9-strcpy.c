07e3e542ff03a1931d121be21f751ca0ef86b483
Hardbass1991 <midailsorridente@gmail.com>
2022-06-13 20:52:36 -0700

    added .c script that copies the content from one array to another

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+/**
+ * *_strcpy -  returns a pointer that contains an input string
+ * @dest: input empty string to store content
+ * @src: input content to store in empty variable dest
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}

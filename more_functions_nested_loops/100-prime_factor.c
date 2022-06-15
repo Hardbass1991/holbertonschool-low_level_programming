b2dee0445725ec3f2bd2d5f042d0e39042002745
Hardbass1991 <midailsorridente@gmail.com>
2022-06-15 10:59:44 -0700

    added .c script that prints the largest prime factor of a number

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -6,13 +6,22 @@
  */
 int main(void)
 {
-	long int i;
+	long int i, j, n, prime;
 
 	for (i = 2; i < 612852475143 / 2; i++)
 		if (612852475143 % i == 0)
 		{
-			printf("%ld\n", 612852475143 / i);
-			break;
+			n = 612852475143 / i;
+			printf("%ld", n);
+			prime = 1;
+			for (j = 2; j < n / 2; j++)
+				if (n % j == 0)
+				{
+					prime = 0;
+					break;
+				}
+			if (prime == 1)
+				printf("%ld", n);
 		}
 	return (0);
 }

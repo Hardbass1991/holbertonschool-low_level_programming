d0e0a69a565c6584ea2920b46c560720bae025af
Hardbass1991 <midailsorridente@gmail.com>
2022-06-15 10:24:05 -0700

    added .c script that finds and prints the largest prime factor of 612852475143

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -5,13 +5,13 @@
  * Return: void
  */
 int main(void)
-{	
+{
 	long int i;
 
 	for (i = 2; i < 612852475143 / 2; i++)
 		if (612852475143 % i == 0)
 		{
-			printf("%ld\n",612852475143 / i);
+			printf("%ld\n", 612852475143 / i);
 			break;
 		}
 	return (0);

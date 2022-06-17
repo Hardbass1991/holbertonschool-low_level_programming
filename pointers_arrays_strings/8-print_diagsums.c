d876c81aefaaebd8d040519e08c086e9b39ee2c6
Hardbass1991 <midailsorridente@gmail.com>
2022-06-16 20:23:31 -0700

    added .c script that prints the sums of the two diagonals of an input square array

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,11 +9,11 @@ void print_diagsums(int *a, int size)
 {
 	int i, d1 = 0, d2 = 0;
 
-	for (i = 0; i < size * size; i+= size + 1)
+	for (i = 0; i < size * size; i += size + 1)
 	{
 		d1 += a[i];
 	}
-	for (i = size - 1; i < size * size - 1; i+= size - 1)
+	for (i = size - 1; i < size * size - 1; i += size - 1)
 	{
 		d2 += a[i];
 	}

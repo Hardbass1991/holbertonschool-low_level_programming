1afe45394607191e0ada77fa114c5af4818d5544
Hardbass1991 <midailsorridente@gmail.com>
2022-06-20 09:45:22 -0700

    added .c script that returns the length of an input string, recursively

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: input string
+ *
+ * Return: length of input string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] == '\0')
+		return (0);
+	return (1 + _strlen_recursion(&s[1]));
+}

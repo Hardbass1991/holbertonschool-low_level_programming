4606fdbf93d968e9f7cde73481022a21ce9f4306
Hardbass1991 <midailsorridente@gmail.com>
2022-06-22 17:20:32 -0700

    added .c script that initializes array with an input character, using malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,15 +10,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *array;
+	unsigned int i;
+
 	if (size == 0)
 		return (NULL);
-	char *array;
-
 	array = malloc(sizeof(c) * size);
 	if (!array)
 		return (NULL);
-	unsigned int i;
-
 	for (i = 0; i < size; i++)
 		array[i] = c;
 	return (array);

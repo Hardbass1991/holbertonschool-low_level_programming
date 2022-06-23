7980ecd5dbe339b23a9fa19e38ab9f2237a68dbe
Hardbass1991 <midailsorridente@gmail.com>
2022-06-22 19:20:55 -0700

    added .c script that concatenates two input strings, using malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,20 +10,24 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *array;
-	int n, n1 = 0, n2 = 0, i;
+	int n, n1 = 0, n2 = 0, i = 0;
 
-	while (s1[n1])
-		n1++;
-	while (s2[n2])
-		n2++;
+	if (s1 != NULL)
+		while (s1[n1])
+			n1++;
+	if (s2 != NULL)
+		while (s2[n2])
+			n2++;
 	n = n1 + n2;
 	array = malloc(sizeof(char) * (n + 1));
 	if (!array)
 		return (NULL);
-	for (i = 0; i <= n; i++)
-		if (i < n1)
+	if (n1)
+		for (i = 0; i < n1; i++)
 			array[i] = s1[i];
-		else
+	if (n2)
+		for (i = n1; i < n1 + n2; i++)
 			array[i] = s2[i - n1];
+	array[i] = '\0';
 	return (array);
 }

6f5d395a6ecaba3747c165ba0041970eca92873a
Hardbass1991 <midailsorridente@gmail.com>
2022-06-22 20:53:39 -0700

    added .c script that creates a two-dimensional array with input dimensions, using malloc

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -15,13 +15,10 @@ int **alloc_grid(int width, int height)
 	if (height <= 0 || width <= 0)
 		return (NULL);
 	array = (int **)malloc(height * sizeof(int *));
+	if (!array)
+		return (NULL);
 	for (i = 0; i < height; i++)
 		array[i] = (int *)malloc(width * sizeof(int));
-	if (!array)
-	{
-		free(array);
-		return (NULL);
-	}
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			array[i][j] = 0;

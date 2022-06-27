81b52e4d93574e0dd1c93931eb131dd2a8a753cf
Hardbass1991 <midailsorridente@gmail.com>
2022-06-27 10:09:29 -0700

    added .c script that allocates memory

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,10 @@
 #include <stdlib.h>
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: input size of malloc
+ *
+ * Return: pointer to allocated memory
+ */
 void *malloc_checked(unsigned int b)
 {
 	char *ptr;

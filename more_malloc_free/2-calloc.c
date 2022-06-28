4075c415328c6c521283e69b8ce154d665ea9152
Hardbass1991 <midailsorridente@gmail.com>
2022-06-27 17:18:05 -0700

    added .c script that allocates memory for an array

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: input number of elements of array
+ * @size: input size of every element
+ *
+ * Return: generated array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char **array;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	array = malloc(nmemb * sizeof(char *));
+	if (!array)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		array[i] = malloc(size);
+		array[i] = 0;
+	}
+	return (array);
+}

9d006dba0686f73bd1cf66f2c2aa9d261f797776
Hardbass1991 <midailsorridente@gmail.com>
2022-06-28 19:56:08 -0700

    added .c script that cretes a new instance of structure dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,22 +14,17 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p;
-	char *name_, *owner_;
 	
-	name_ = malloc(sizeof(name));
-	owner_ = malloc(sizeof(owner));
-	strcpy(name_, name);
-	strcpy(owner_, owner);
-	/**printf("%s\n", name_);
-	printf("%s\n", owner_);*/
 	p = malloc(sizeof(dog_t));
 	if (!p)
 	{
 		free(p);
 		return (NULL);
 	}
-	p->name = name_;
+	p->name = malloc(sizeof(char *));
+	p->owner = malloc(sizeof(char *));
+	strcpy(p->name, name);
+	strcpy(p->owner, owner);
 	p->age = age;
-	p->owner = owner_;
 	return (p);
 }

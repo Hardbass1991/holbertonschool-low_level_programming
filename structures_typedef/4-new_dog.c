202689edf206945dd1bfbe106f7d06dbab474e15
Hardbass1991 <midailsorridente@gmail.com>
2022-06-28 19:00:25 -0700

    added .c script that cretes a new instance of structure dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "dog.h"
+/**
+ * new_dog - creates an instance of the structure dog
+ * @name: input name for new structure
+ * @age: input age for new structure
+ * @owner: input owner for new structure
+ *
+ * Return: pointer to created instance of dog, or NULL if that fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+
+	p = malloc(sizeof(dog_t));
+	if (!p)
+		return (NULL);
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
+}

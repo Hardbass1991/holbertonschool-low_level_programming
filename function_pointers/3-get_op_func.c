da8ae909a77b6a441d184e6900e98777c3a0051c
Hardbass1991 <midailsorridente@gmail.com>
2022-06-29 17:37:48 -0700

    added .c scripts for a program that performs basic arithmetic operations

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,6 +1,13 @@
 #include "3-calc.h"
 #include <stddef.h>
 #include <string.h>
+/**
+ * get_op_func - receives a char (the sign of an operation)
+ * and returns a function that performs such operation
+ * @s: input char (sign of operation)
+ *
+ * Return: pointer to function
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -12,11 +19,12 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 	int i;
+
 	i = 0;
 	while (i < 5)
 	{
 		if (!strcmp(ops[i].op, s))
-		{			
+		{
 			return (ops[i].f);
 		}
 		i++;
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+/**
+ * main - command performs basic arithmetic operations relying on
+ * other functions
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: result of operation
+ */
 int main(int argc, char *argv[])
 {
 	op_t result;
@@ -13,7 +21,7 @@ int main(int argc, char *argv[])
 			printf("Error\n");
 			exit(99);
 		}
-		if (!((strcmp(argv[2], "/") || strcmp(argv[2], "%")) && strcmp(argv[3], "0")))
+		if (!(((argv[2][0] != '/') || strcmp(argv[2], "%")) && strcmp(argv[3], "0")))
 		{
 			printf("Error\n");
 			exit(100);
@@ -22,7 +30,7 @@ int main(int argc, char *argv[])
 		result.f = get_op_func(argv[2]);
 		printf("%d\n", result.f(atoi(argv[1]), atoi(argv[3])));
 		return (0);
-	}	
+	}
 	else
 	{
 		printf("Error\n");

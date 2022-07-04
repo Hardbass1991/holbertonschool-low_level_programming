e85223080b95c3668d8b435eca049bff90621d06
Hardbass1991 <midailsorridente@gmail.com>
2022-07-04 11:19:49 -0700

    added .c script that prints input strings, separated by input separator

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_strings - prints strings followed by a new line
+ * @separator: input string that will be used as separator
+ * @n: number of strings that will be given as input
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+	char *a;
+
+	if (!separator)
+		separator = "";
+	if (n > 0)
+	{
+		va_start(ap, n);
+		printf("%s", va_arg(ap, char*));
+		for (i = 1; i < n; i++)
+		{
+			a = va_arg(ap, char*);
+			if (!a)
+				printf("%sGAAA", separator);
+			else
+				printf("%s%s", separator, a);
+		}
+		va_end(ap);
+	}
+	printf("\n");
+}

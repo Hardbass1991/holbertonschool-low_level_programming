28e6b1b9c35268cdee605996e66273d9a8bf53f0
Hardbass1991 <midailsorridente@gmail.com>
2022-07-04 10:36:04 -0700

    added .c script that prints input numbers, using an input separator

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: input string that will be used as separator
+ * @n: number of ints that will be given as input
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+	printf("%d", va_arg(ap, int));
+	for (i = 1; i < n; i++)
+		printf("%s%d", separator, va_arg(ap, int));
+	printf("\n");
+	va_end(ap);
+}

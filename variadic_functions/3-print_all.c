35d381a2b5e2360716ea802253c9e479a1a1cdcc
Hardbass1991 <midailsorridente@gmail.com>
2022-07-04 21:39:29 -0700

    added .c script that prints every argument passed, according to input format string

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,30 +2,47 @@
 #include <stdarg.h>
 #include <string.h>
 #include <stdlib.h>
+/**
+ * print_all - prints everyargument passe according to input format string
+ * @format: input format string
+ */
 void print_all(const char * const format, ...)
 {
-	int i = 0, n;
-	char *f, *fmt;
+	int i = 0, n, val_char;
 	va_list ap;
+	char *tmp;
 
 	n = strlen(format);
-	f = malloc(n + 1);
-	strcpy(f, format);
-	fmt = malloc(3);
-
 	va_start(ap, format);
-	while (i < n)
+	while (format && i < n)
 	{
-		if (f[i] == 'c' || f[i] == 'f' || f[i] == 'i' || f[i] == 's')
+		val_char = 0;
+		switch (format[i])
 		{
-			if (i)
-				printf(", ");
-			fmt[0] = '%';
-			fmt[1] = f[i];
-			fmt[2] = '\0';
-			printf(fmt, va_arg(ap, char*));
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				val_char = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				val_char = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				val_char = 1;
+				break;
+			case 's':
+				tmp = va_arg(ap, char*);
+				if (!tmp)
+					tmp = "(nil)";
+				printf("%s", tmp);
+				val_char = 1;
+				break;
 		}
+		if (i < n - 1 && val_char)
+			printf(", ");
 		i++;
 	}
+	va_end(ap);
 	printf("\n");
 }

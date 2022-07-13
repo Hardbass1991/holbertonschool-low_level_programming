a380a20dadfc9af75d87368f9332c10b3687afae
Hardbass1991 <midailsorridente@gmail.com>
2022-07-13 13:32:03 -0700

    added .c script that adds a new node at the end of an input linked list

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_nodeint_end - adds a node at the end of an input linked list
+ * @head: pointer to input linked list
+ * @n: value of node that will be added
+ *
+ * Return: pointer to new element
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node, *last;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (!*head)
+	{
+		(*head) = new_node;
+		return (*head);
+	}
+	last = *head;
+	while (last->next)
+		last = last->next;
+	last->next = new_node;
+	return (last->next);
+}

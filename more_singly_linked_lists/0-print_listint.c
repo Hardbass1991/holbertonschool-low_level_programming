a52d0edd409650d0c6c2e9f9ffb184cb70ecad89
Hardbass1991 <midailsorridente@gmail.com>
2022-07-13 12:59:32 -0700

    added function description

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include "lists.h"
+/** print_listint - prints int values in a linked list
+ * @h: input linked list
+ *
+ * Return: number of elements in input list
+ */
 size_t print_listint(const listint_t *h)
 {
 	int n = 0;

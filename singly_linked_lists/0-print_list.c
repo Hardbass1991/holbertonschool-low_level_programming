a6cae81b5b1d07deb4dcdb990ec439de176a109c
Hardbass1991 <midailsorridente@gmail.com>
2022-07-12 17:04:13 -0700

    added function description

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,11 +1,17 @@
 #include "lists.h"
 #include <stdio.h>
+/**
+ * print_list - prints elements from input linked list
+ * @n: pointer to linked list
+ *
+ * Return: number of elements in linked list
+ */
 size_t print_list(const list_t *h)
 {
 	int n = 0;
 
 	while (h)
-	{	
+	{
 		n += 1;
 		if (!h->str)
 			printf("[0] (nil)\n");

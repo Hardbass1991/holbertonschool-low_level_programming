c37ddffb0baf0b826cd7f511fc3322ee826a05f4
Hardbass1991 <midailsorridente@gmail.com>
2022-07-13 14:03:27 -0700

    added .c script that frees an input linked list, getting a pointer to the head (and not the head itself) as input

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - frees an input linked list.
+ * @head: pointer to input linked list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *curr_node;
+
+	while (*head)
+	{
+		curr_node = *head;
+		*head = (*head)->next;
+		free(curr_node);
+	}
+	*head = NULL;
+}

66bac1980fb8acd8eee754e22976ea7e89dfd24b
Hardbass1991 <midailsorridente@gmail.com>
2022-07-13 14:58:00 -0700

    deleted apparently useless free statement

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -1,16 +1,21 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include "lists.h"
+/**
+ * pop_listint - pops node from beginning of input linked list
+ * @head: pointer to input linked list
+ *
+ * Return: value of element popped, or 0 if it fails
+ */
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp;
 	int tmp_n;
 
-	if (!head)
+	if (!(*head))
 		return (0);
 	tmp = (*head);
-	tmp_n = (*head)->n;
-	free(tmp);
+	tmp_n = tmp->n;
 	(*head) = (*head)->next;
 	return (tmp_n);
 }

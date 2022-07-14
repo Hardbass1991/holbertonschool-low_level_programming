8c9f2802154e8d278a9ea3f68de14514a66396b8
Hardbass1991 <midailsorridente@gmail.com>
2022-07-13 20:39:50 -0700

    added script that inserts node at input index of input linked list

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - inserts a node in index idx  input linked list
+ * @head: pointer to input linked list
+ * @idx: position where new node will be inserted
+ * @n: data to be inserted at new node
+ *
+ * Return: inserted node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *tmp, *ptr;
+
+	ptr = malloc(sizeof(listint_t));
+	ptr = *head;
+	tmp = malloc(sizeof(listint_t));
+	tmp->n = n;
+	if (!(*head))
+		return (NULL);
+	while (ptr->next && i < idx - 1)
+	{
+		ptr = ptr->next;
+		i += 1;
+	}
+	if (idx > i + 1)
+		return (NULL);
+	tmp->next = ptr->next;
+	ptr->next = tmp;
+	return (ptr->next);
+}

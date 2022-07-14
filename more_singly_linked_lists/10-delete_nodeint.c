995e2c25be0912f38a66fddfa74af1520d524347
Hardbass1991 <midailsorridente@gmail.com>
2022-07-13 21:55:18 -0700

    added script that deletes nodes in input index of input linked list

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,37 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes a node in input index of linked list
+ * @head: pointer to input linked list
+ * @index: position where new node will be inserted
+ *
+ * Return: 1 if it succeeded, -1 otherwise
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *ptr;
+	
+	if (!(*head))
+		return (-1);
+	ptr = malloc(sizeof(listint_t));
+	if (!ptr)
+		return (-1);
+	if (index == 0)
+	{
+		(*head) = (*head)->next;
+		return (-1);
+	}
+	ptr = (*head);
+	while (ptr->next && i < index - 1)
+	{
+		ptr = ptr->next;
+		i += 1;
+	}
+	if (index > i + 1)
+		return (-1);
+	ptr->next = ptr->next->next;
+	return (1);
+}

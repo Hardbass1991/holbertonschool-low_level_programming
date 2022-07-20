369cefa706741759ac4cae92a025ad29c78306c1
Hardbass1991 <midailsorridente@gmail.com>
2022-07-20 13:57:04 -0700

    corrected number of characters to be written

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 	{
-		wr = write(fd, text_content, strlen(filename));
+		wr = write(fd, text_content, strlen(text_content));
 		if (wr < 0)
 			return (-1);
 	}

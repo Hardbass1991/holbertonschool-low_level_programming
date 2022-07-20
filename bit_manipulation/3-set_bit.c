bca7d7ab350f3e843bc598720638d6f8eb6f4d0e
Hardbass1991 <midailsorridente@gmail.com>
2022-07-19 19:25:02 -0700

    added script that sets the value of a bit to 1 at a given index.

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * set_bit - sets the value of a bit to 1 at a given index.
+ * @n: pointer to input lu
+ * @index: position at which an 1 will be placed
+ *
+ * Return: altered number
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int one = 1;
+	
+	if (index >= sizeof(*n) * 8)
+		return (-1);
+	one <<= index;
+	*n = (*n | one);
+	return (*n);
+}

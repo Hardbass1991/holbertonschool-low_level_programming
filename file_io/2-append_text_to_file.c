0f528fe488b8e8ac902c0587b7221c6256a517af
Hardbass1991 <midailsorridente@gmail.com>
2022-07-20 14:38:26 -0700

    added script that appends input string to existing file

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+/**
+ * create_file - creates a text file and write in it
+ * @filename: string with the name of the file to be used
+ * @text_content: string to be written in the file
+ *
+ * Return: 1 if successful, or -1 otherwise
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, wr;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_RDWR | O_APPEND, 0600);
+	if (fd < 0)
+		return (-1);
+	if (text_content)
+	{
+		wr = write(fd, text_content, strlen(text_content));
+		if (wr < 0)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}

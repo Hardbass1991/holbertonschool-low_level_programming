3325f917fa0f425d1089e885bc33162c9e793a43
Hardbass1991 <midailsorridente@gmail.com>
2022-07-20 13:02:12 -0700

    added script that reads from a file and prints it to stdout

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, rd, wr;
+	char *buf;
+
+	buf = malloc(letters + 1);
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY, 0400);
+	if (fd < 0)
+		return (0);
+	rd = read(fd, buf, letters);
+	if (rd < 0)
+		return (0);
+	buf[letters] = '\0';
+	wr = write(1, buf, rd);
+	if (wr < 0)
+		return (0);
+	return(wr);
+}
---
+++ file_io/main.h
@@ -0,0 +1,5 @@
+#ifndef FUNCS
+#define FUNCS
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif

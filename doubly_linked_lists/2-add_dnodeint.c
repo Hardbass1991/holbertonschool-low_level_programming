db9094cf3cc291f0fbab2cc94b874b1f3f1c5738
Hardbass1991 <midailsorridente@gmail.com>
2022-07-21 10:21:01 -0700

    added script that adds a new node at the beginning of a doubly linked list

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
+ * @head: pointer to the head of the list
+ * @n: value to be given to the new node
+ *
+ * Return: pointer to new element, or NULL if process failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = (*head);
+	(*head) = new_node;
+	return (*head);
+}

8f692da2d1aae67328b1ecef004c15828c83fcf9
Hardbass1991 <midailsorridente@gmail.com>
2022-07-21 13:07:05 -0700

    added script that retrieves node at input index of input doubly linked list

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * get_dnodeint_at_index - returns a node whose index is given as input
+ * @head: head of the list
+ * @index: position of the node that will be returned
+ *
+ * Return: node at input index of linked list
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *ptr;
+	unsigned int i = 0;
+
+	ptr = head;
+	while (ptr->next && i < index)
+	{
+		ptr = ptr->next;
+		i++;
+	}
+	if (index > i)
+		return (NULL);
+	return (ptr);
+}

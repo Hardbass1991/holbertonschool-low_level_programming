c6916b62f174aaaf26f06a2640c902672e9c29fa
Hardbass1991 <midailsorridente@gmail.com>
2022-07-21 14:02:16 -0700

    added script that inserts node into input doubly linked lists

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,41 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts a node in index idx input linked list
+ * @h: pointer to input linked list
+ * @idx: position where new node will be inserted
+ * @n: data to be inserted at new node
+ *
+ * Return: inserted node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	dlistint_t *tmp, *ptr;
+
+	ptr = malloc(sizeof(dlistint_t));
+	if (!ptr)
+		return (NULL);
+	ptr = *h;
+	tmp = malloc(sizeof(dlistint_t));
+	if (!tmp)
+		return (NULL);
+	tmp->n = n;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	else if (!(*h))
+		return (NULL);
+	while (ptr->next && i < idx - 1)
+	{
+		ptr = ptr->next;
+		i += 1;
+	}
+	if (idx > i + 1)
+		return (NULL);
+	tmp->next = ptr->next;
+	tmp->prev = ptr;
+	ptr->next = tmp;
+	return (ptr->next);
+}

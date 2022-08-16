41e7ad51043bd05f7ce110db571fffc6b2e312b0
Hardbass1991 <midailsorridente@gmail.com>
2022-08-16 10:31:15 -0700

    added script that creates a hash table, and the header file

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of hash table (number of nodes)
+ *
+ * Return: newly created hash table, or NULL if process fails
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash_table;
+
+	new_hash_table = malloc(sizeof(hash_table_t));
+	if (!new_hash_table)
+		return (NULL);
+
+	new_hash_table->array = malloc(size * 8);
+	if (!new_hash_table->array)
+		return (NULL);
+	return (new_hash_table);
+}

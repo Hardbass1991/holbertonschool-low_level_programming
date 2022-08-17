8502f936d2f67f1526fdbcece27bb136c5a2723b
Hardbass1991 <midailsorridente@gmail.com>
2022-08-17 08:48:32 -0700

    added script that prints items from a hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "hash_tables.h"
+void hash_table_print(const hash_table_t *ht)
+{
+	int i, j = 0, n;
+	hash_node_t *item;
+
+	n = 1024;
+
+	if (!ht)
+		return;
+	printf("%d\n", n);
+	putchar('{');
+	for (i = 0; i < n; i++)
+	{
+		item = ht->array[i];
+		if (item)
+		{
+			if (j)
+				printf(", ");
+			printf("\'%s\': \'%s\'", item->key, item->value);
+			j++;
+		}
+	}
+	printf("}\n");
+}

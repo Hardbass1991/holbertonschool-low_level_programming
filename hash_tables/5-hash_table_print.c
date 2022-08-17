c3423b8dfd7b06c186477e4e607b09a6134c44a8
Hardbass1991 <midailsorridente@gmail.com>
2022-08-17 08:56:40 -0700

    added function description

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include "hash_tables.h"
+/**
+ * hash_table_print - prints out elements of a hash table
+ * @ht: input hash table
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	int i, j = 0, n;
@@ -9,7 +13,7 @@ void hash_table_print(const hash_table_t *ht)
 
 	if (!ht)
 		return;
-	printf("%d\n", n);
+	/*printf("%d\n", n);*/
 	putchar('{');
 	for (i = 0; i < n; i++)
 	{

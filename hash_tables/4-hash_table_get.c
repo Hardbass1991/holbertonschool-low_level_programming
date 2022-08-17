87325f94bdf22d02ddb6b996f75baf568fc1afaa
Hardbass1991 <midailsorridente@gmail.com>
2022-08-16 19:35:04 -0700

    added script that retrieves the value associated with an input key

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include "hash_tables.h"
+/**
+ * hash_table_get - gets the value that matches an input key
+ * @ht: hash able where key will be searched
+ * @key: key that will be searched in ht
+ *
+ * Return: value that matches key if successful, or NUll otherwise
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *item;
+	unsigned long int index;
+
+	index = key_index((unsigned char *)key, ht->size);
+	item = (ht->array[index]);
+	if (!item)
+		return (NULL);
+	return (item->value);
+}
